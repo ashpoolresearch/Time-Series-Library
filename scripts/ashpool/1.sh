545d2aa12d4b24c0a491d3f9a6fbbda30762fe4b
ashpoolresearch <research@ashpoolresearch.com>
2024-01-20 12:15:18 +1100

    Change sh file

--- scripts/ashpool/1.sh
+++ scripts/ashpool/1.sh
@@ -4,20 +4,23 @@ model_name=Transformer
 python -u run.py \
   --task_name long_term_forecast \
   --is_training 1 \
-  --root_path ./dataset/btc5m/ \
+  --root_path ./dataset/ \
   --data_path BTC_5m.csv \
-  --model_id Exchange_96_96 \
+  --model_id BTC_96_96_S \
   --model $model_name \
   --data custom \
-  --features M \
+  --features S \
   --seq_len 96 \
   --label_len 48 \
   --pred_len 96 \
-  --e_layers 2 \
+  --e_layers 5 \
   --d_layers 1 \
   --factor 3 \
   --enc_in 8 \
   --dec_in 8 \
   --c_out 8 \
   --des 'Exp' \
-  --itr 1 
+  --itr 1 \
+  --target Close \
+  --train_epochs 100 
+  
